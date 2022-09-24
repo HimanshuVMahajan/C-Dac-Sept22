225bc1dd806c59891f5e129ffd64962444749989
HimanshuVMahajan <113438978+HimanshuVMahajan@users.noreply.github.com>
2022-09-24 17:27:19 +0530

    Add files via upload

---
+++ Assignment2/Page 1/P17.java
@@ -0,0 +1,31 @@
+class P17
+{
+
+public static void main(String args[])
+{
+	int i;
+	int j;
+	
+int n=1;
+for(i=1;i<=5;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+	System.out.print(n+" ");
+	n++;
+	
+	
+	}
+	System.out.println("");
+}
+
+
+
+}
+
+
+
+
+
+
+}
